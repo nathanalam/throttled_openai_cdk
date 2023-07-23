fc40ce2194ae83aefb6d19f6535e542d04eb0ab2
Nathan Alam <nathanalam99@gmail.com>
2023-07-22 21:02:17 -0400

    Initial commit

---
+++ test/throttled_ml_api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ThrottledMlApi from '../lib/throttled_ml_api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/throttled_ml_api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ThrottledMlApi.ThrottledMlApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
